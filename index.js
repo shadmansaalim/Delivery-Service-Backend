dcfcc36c3d5b73c7f19858cd607d369fe3aab6bb
shadmansaalim321 <shadmansaalim321@gmail.com>
2021-10-29 03:29:09 +0600

    MongoDB added

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
+//MongoDB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yuk1u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 
