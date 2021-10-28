61423b1d78f25a122d8b0709c58c275d17234606
shadmansaalim321 <shadmansaalim321@gmail.com>
2021-10-29 03:31:30 +0600

    Async func written

--- index.js
+++ index.js
@@ -17,6 +17,17 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yuk1u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("delivery_service");
+        const servicesCollection = database.collection("services");
+    }
+    finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
